8be16c8d0151661023d18010951f5d99f83081c1
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-08-05 13:38:26 +0300

    DONE

---
+++ Modul3HW3/Modul3HW3/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modul3HW3
+{
+    public class Class1
+    {
+        public Action<bool> ShowDelegate { get; set; }
+
+        public int Pow(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+    }
+}
---
+++ Modul3HW3/Modul3HW3/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Modul3HW3
+{
+    public class Class2
+    {
+        private int _result;
+        public Func<int, int, int> PowDelegate { get; set; }
+
+        public Predicate<int> ResultDelegate { get; set; }
+
+        public Predicate<int> Calc(Func<int, int, int> value, int a, int b)
+        {
+            _result = value(a, b);
+            ResultDelegate = Result;
+
+            return ResultDelegate;
+        }
+
+        public bool Result(int value)
+        {
+            if (_result % value == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
